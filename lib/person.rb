50d24a276cb01a24a7a8617121510003f252b0bb
George Blackmore <gtblackmore@gmail.com>
2019-12-20 01:24:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize
+    @name = name
+    @job = job
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job_type)
+    @job = job_type
+  end
+end
