3a6bbb55d4f4ff8e10b1359a90c6a8af5cd2c704
Nguyen Ha <nynha0526@gmail.com>
2020-01-09 06:01:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,40 +1,20 @@
 class Dog 
   
-  def initialize(name)
-    @name = name
-  end
-  
   def name
     @name
   end
   
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
   
-end
-
-class Breed 
-  
-  def initialize(breed)
-    @breed = breed
-  end
   
   def breed
     @breed
   end
   
-  def breed=(breed_type)
-    @breed = breed_type
+  def breed=(breed)
+    @breed = breed
   end
   
 end
-
-bruno = Dog.new 
-bruno.name = "Bruno"
-
-rottwieler = Breed.new 
-rottwieler.breed = "Rottwieler"
-
-puts bruno.name
-puts rottwieler.breed 
