5bbc7aa7fdb6b5d6ec0b6bf859f484002484da8b
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-01-16 15:47:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,40 @@
+class Dog
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+end
+
+sunday = Dog.new("Sunday")
+sunday.name 
+
+end
+
+class Dog
+  def initialize(breed)
+    @breed = breed_name
+  end
+  
+  def breed_name
+    @breed
+  end
+  
+  def breed_name=(new_breed_name)
+    @name = new_breed_name
+  end
+end
+
+labrador = Dog.new("labrador")
+labrador.breed
+
+  
+  
+
